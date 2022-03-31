5a4f6c94e4af27c9a7f3912d65c9f20ed0c2ced3
Satyam1Deepu <73704981+Satyam1Deepu@users.noreply.github.com>
2022-03-31 20:40:14 +0530

    Create worksheet1Q3.py

---
+++ Chandigarh_University_PROGRAMMING_IN_PYTHON_LAB/Worksheet1.1/worksheet1Q3.py
@@ -0,0 +1,20 @@
+""""कर्मण्ये वाधिका रस्ते मा फलेषु कदाचन।
+मा कर्म फल हेतु र्भूर्मा ते सङ्गोऽस्त्व कर्मणि॥"""
+
+print("Enter Length (Unit : cm ) : ")
+length = float(input())
+# Converting :
+print("In meters : ", (length / 100))
+print("In Kilometers : ", (length / 1000000))
+
+print("Enter Length (Unit : m ) : ")
+length = float(input())
+# Converting :
+print("In centimeter: ", (length*100))
+print("In Kilometers : ", (length / 1000))
+
+print("Enter Length (Unit : Km ) : ")
+length = float(input())
+# Converting :
+print("In Centimeters : ", (length*1000000))
+print("In meters : ", (length*1000))
