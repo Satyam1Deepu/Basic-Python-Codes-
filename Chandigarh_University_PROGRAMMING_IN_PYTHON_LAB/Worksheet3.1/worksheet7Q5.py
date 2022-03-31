9061344408924ff1de7c0625d1caa2c709c634f0
Satyam1Deepu <73704981+Satyam1Deepu@users.noreply.github.com>
2022-03-31 15:03:56 +0530

    Create worksheet7Q5.py

---
+++ Chandigarh_University_PROGRAMMING_IN_PYTHON_LAB/Worksheet3.1/worksheet7Q5.py
@@ -0,0 +1,22 @@
+"""Write a Python program to check if a specified element presents in a tuple of tuples"""
+
+""""कर्मण्ये वाधिका रस्ते मा फलेषु कदाचन।
+मा कर्म फल हेतु र्भूर्मा ते सङ्गोऽस्त्व कर्मणि॥"""
+
+def isFound(tup,element):
+    found = False
+    for i in range(0,len(collection)):
+        for j in range(0,len(collection[i])):
+            if(element == collection[i][j]):
+               found = True
+               return found
+
+
+collection = ((2,75,45,96,15),(1,2,4,9,8,3,7),(67,81,34,97,91),(12,76,18,43,63))
+
+number = int(input("Enter number to remove from tuple of tuples :"))
+
+if(isFound(collection,number)):
+    print("Number found !!! ")
+else:
+    print("Number not found :(")
