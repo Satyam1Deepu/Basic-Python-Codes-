e397a3752735a0a280cc66bcdb106bebeba52291
Satyam1Deepu <73704981+Satyam1Deepu@users.noreply.github.com>
2022-03-31 13:29:36 +0530

    Create worksheet2Q3.py

---
+++ Chandigarh_University_PROGRAMMING_IN_PYTHON_LAB/Worksheet2/worksheet2Q3.py
@@ -0,0 +1,16 @@
+"""Python Program to Take three numbers from the user and print the greatest number"""
+
+""""कर्मण्ये वाधिका रस्ते मा फलेषु कदाचन।
+मा कर्म फल हेतु र्भूर्मा ते सङ्गोऽस्त्व कर्मणि॥"""
+
+print(" Satyam Kumar deepu UID : 20BCS9274")
+print("Enter Three Numbers :")
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+if(num1 > num2 and num1 > num3):
+    print("Greatest Number : ",num1)
+elif(num2 > num1 and num2 > num3):
+    print("Greatest Number : ", num2)
+else:
+    print("Greatest Number : ",num3)
