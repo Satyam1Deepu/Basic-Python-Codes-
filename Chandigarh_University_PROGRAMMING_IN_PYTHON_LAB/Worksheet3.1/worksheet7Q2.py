a3d755634691564a6a8bd3d08ba15959967fc5f3
Satyam1Deepu <73704981+Satyam1Deepu@users.noreply.github.com>
2022-03-31 14:13:36 +0530

    Create worksheet7Q2.py

---
+++ Chandigarh_University_PROGRAMMING_IN_PYTHON_LAB/Worksheet3.1/worksheet7Q2.py
@@ -0,0 +1,15 @@
+""" Write a Python program to remove an empty tuple(s) from a list of tuples."""
+
+""""कर्मण्ये वाधिका रस्ते मा फलेषु कदाचन।
+मा कर्म फल हेतु र्भूर्मा ते सङ्गोऽस्त्व कर्मणि॥"""
+
+li = [(2,75,45,96,15),(),(1,2,4,9,8,3,7),(67,81,34,97,91),(),(12,76,18,43,63)]
+print(" List of tuples before :")
+print(li)
+i = 0
+while(i < len(li)):
+    if(len(li[i]) == 0):
+        del li[i]
+    i += 1
+print(" List of tuples after :")
+print(li)
