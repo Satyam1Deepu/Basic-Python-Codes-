31f086ce8e6dd260d0ae30c4576ce24c5752ae3c
Satyam1Deepu <73704981+Satyam1Deepu@users.noreply.github.com>
2022-03-31 21:14:51 +0530

    Create worksheet1Q1.py

---
+++ Chandigarh_University_PROGRAMMING_IN_PYTHON_LAB/Worksheet1.1/worksheet1Q1.py
@@ -0,0 +1,14 @@
+""""कर्मण्ये वाधिका रस्ते मा फलेषु कदाचन।
+मा कर्म फल हेतु र्भूर्मा ते सङ्गोऽस्त्व कर्मणि॥"""
+  
+print("Enter Two integer value : ")
+var1 = int(input())
+var2 = int(input())
+
+print("Adding two operands : ", var1 +var2)
+print("Subtracting two operands : ", var1 - var2)
+print("Multiplies two operands : ", var1*var2)
+print("Division (float): divides the first operand by the second : ", var1/var2)
+print("Division (floor): divides the first operand by the second : ", var1//var2)
+print("Remainder when first operand is divided by the second : ", var1 %var2)
+print("Power : Returns first raised to power second : ", var1**var2)
