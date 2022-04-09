83fe7f299d29403f1d5f1f7208e334a5e04d6a26
Satyam1Deepu <73704981+Satyam1Deepu@users.noreply.github.com>
2022-04-09 21:44:29 +0530

    Create worksheet2.4Q1.py

---
+++ Chandigarh_University_PROGRAMMING_IN_PYTHON_LAB/Worksheet2.4/Changed Question/worksheet2.4Q1.py
@@ -0,0 +1,14 @@
+""""कर्मण्ये वाधिका रस्ते मा फलेषु कदाचन।
+मा कर्म फल हेतु र्भूर्मा ते सङ्गोऽस्त्व कर्मणि॥"""
+
+"""Write a python program using tuple constructor to
+ create/convert given sequence to tuples."""
+
+print("\nSatyam Kumar Deepu : 20BCS9274\n")
+tup = [1,2,3,4,5,6,7,8,9]
+tup1 = tuple([(1,2),(3,4),(5,6)])
+print("Type of tup before :" , type(tup))
+# Using tuple constructor to convert list into tuple
+tup = tuple(tup)
+print("Type of tup after :" , type(tup))
+print("Type of tup1 :" , type(tup))
